3aa0bbfc135d58ba342e57f51ec80a89cd5f4fa1
Patricia <patriciastarck@gmail.com>
2025-05-13 10:46:05 -0300

    feat: adicionar classe ApiCliente para buscar taxas de câmbio

--- src/ApiCliente.java
+++ src/ApiCliente.java
@@ -1,2 +1,26 @@
-public class ApiCliente {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+class ApiCliente {
+
+    private static final String API_KEY = "a733795f7594130fac06e77d";
+
+    public String buscarTaxasDeCambio(String moedaOrigem) throws IOException, InterruptedException {
+        String apiUrl = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/" + moedaOrigem;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Erro ao acessar a API. Código HTTP: " + response.statusCode());
+        }
+
+        return response.body();
+    }
 }
